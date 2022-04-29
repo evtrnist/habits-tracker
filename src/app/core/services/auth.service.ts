1d0b968d4a6a9eeeb88e115053fcd8d6da159247
evtrnist <evagrandet@ro.ru>
2022-04-29 17:53:51 +0300

    adds core module with auth service

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+import * as auth from 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+    AngularFirestore,
+    AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private angularFirestore: AngularFirestore,
+        private angularFireAuth: AngularFireAuth
+    ) {}
+
+    signIn(email: string, password: string) {
+        this.angularFireAuth.signInWithEmailAndPassword(email, password);
+    }
+
+    signUp(email: string, password: string) {
+        // TO DO: send verification email
+        this.angularFireAuth.createUserWithEmailAndPassword(email, password);
+    }
+
+    signOut() {
+        this.angularFireAuth.signOut();
+    }
+}

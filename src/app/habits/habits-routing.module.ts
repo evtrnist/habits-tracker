71f9f23b08c56352ca5e014c1f5c103e6a894246
evtrnist <evagrandet@ro.ru>
2022-06-13 19:11:23 +0300

    adds angularFire auth guard

--- src/app/habits/habits-routing.module.ts
+++ src/app/habits/habits-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        canActivate: [AngularFireAuthGuard],
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
